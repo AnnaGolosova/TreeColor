276ed01bab883e1062ff5e05f854db93ec401865
Anna <golosowa.anna@gmail.com>
2017-12-12 02:11:02 +0300

    Created Settings controller and page for setting up tests.

---
+++ TreeColor/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TreeColor.Controllers
+{
+    public class SettingsController : BaseController
+    {
+        public ActionResult TestList()
+        {
+            return View(DBcontext.Tests.ToList());
+        }
+
+        public ActionResult SettingsTest(int id = 0)
+        {
+            return View();
+        }
+    }
+}
